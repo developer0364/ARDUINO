43184975b4be3ad619964a25b18cbd6683a800d3
Santiago-Molina-Arau <tu-email-de-github@ejemplo.com>
2026-04-14 22:17:54 -0300

    codigo base de trabajo

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
-import { useState } from 'react'
-import './App.css'
-
+import { useNavigate } from 'react-router-dom'; // <--- ¿Tenías esta línea?
+import './App.css';
 
 function App() {
-  // 1. La lógica la definimos como una función real
-  const navigate= useNavigate();
+  const navigate = useNavigate();
+
   const manejarClickJugar = () => {
     navigate('/jugarDisplay');
-    
-    
   };
 
   return (
-    <div className="main-wrapper">
-      <div>
+    <div>
+      <div className="main-wrapper">
         <h1 className="title">BIENVENIDOS A LA TRIVIA</h1>
         <p className="texto">SELECCIONA</p>
 
         <div className="contenedor-botones">
-          {/* 2. Llamamos a la función directamente aquí */}
           <button onClick={manejarClickJugar}>JUGAR</button>
           <button>INSTRUCCIONES</button>
           <button>BLANK</button>
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import JugarDisplay from './jugarDisplay.jsx'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom' // Importante
+import App from './App.jsx'
 
-
-
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/jugarDisplay" element={<JugarDisplay />} />
-    </Routes>
-  </BrowserRouter>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter> 
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
