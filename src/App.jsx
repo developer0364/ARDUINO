283cff75c62c4c9c48df5297c4cbe01b67005713
Santiago-Molina-Arau <tu-email-de-github@ejemplo.com>
2026-04-20 13:46:06 -0300

    cambios lucas 20/4

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
-import { useNavigate } from 'react-router-dom'; // <--- ¿Tenías esta línea?
+import { useNavigate } from 'react-router-dom';
+import JugarDisplay from './jugarDisplay';
 import './App.css';
 
 function App() {
-  const navigate = useNavigate();
+  const navigate = useNavigate(); 
 
   const manejarClickJugar = () => {
-    navigate('/jugarDisplay');
+    navigate('/jugarDisplay'); 
   };
 
   return (
-    <div>
-      <div className="main-wrapper">
-        <h1 className="title">BIENVENIDOS A LA TRIVIA</h1>
-        <p className="texto">SELECCIONA</p>
+    /* El "main-wrapper" es el escenario completo */
+    <div className="main-wrapper">
+      
+      {/* El encabezado principal */} 
+      <h1 className="title">BIENVENIDOS A LA TRIVIA</h1>
+      
+   
+      <p className="texto">SELECCIONA UNA OPCIÓN</p>
 
-        <div className="contenedor-botones">
-          <button onClick={manejarClickJugar}>JUGAR</button>
-          <button>INSTRUCCIONES</button>
-          <button>BLANK</button>
-        </div>
+      {/* Esta es la "bandeja" donde apoyamos los botones */}
+      <div className="contenedor-botones">
+        
+        <button>INSTRUCCIONES</button>
+        <button  onClick={manejarClickJugar}>
+          JUGAR</button>
+        <button >BLANK</button>
       </div>
+
     </div>
   );
 }
-
+// pendiente de alinear botones con texto superior
 export default App;
